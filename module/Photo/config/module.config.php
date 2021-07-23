d6faecd4804627cf020a9a4c740594bd2c864353
Tom Udding <tomudding@users.noreply.github.com>
2021-07-23 17:58:34 +0200

    Update Photo models to use native attributes instead of Doctrine Annotations

--- module/Photo/config/module.config.php
+++ module/Photo/config/module.config.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Photo;
+
+use Application\Extensions\Doctrine\AttributeDriver;
 use Photo\Command\WeeklyPhoto;
 use Photo\Controller\{
     AlbumAdminController,
@@ -455,14 +458,15 @@ return [
     ],
     'doctrine' => [
         'driver' => [
-            'photo_entities' => [
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => [__DIR__ . '/../src/Model/'],
+            __NAMESPACE__ . '_driver' => [
+                'class' => AttributeDriver::class,
+                'paths' => [
+                    __DIR__ . '/../src/Model/',
+                ],
             ],
             'orm_default' => [
                 'drivers' => [
-                    'Photo\Model' => 'photo_entities',
+                    __NAMESPACE__ . '\Model' => __NAMESPACE__ . '_driver',
                 ],
             ],
         ],
